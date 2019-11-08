32306a6312d43b161938ceb300feacca1986f579
Gjoko <temelkovgjoko@gmail.com>
2019-11-08 16:59:12 +0100

    person  two-way databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
+import { FormsModule } from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/persons/person-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
+})
+
+export class PersonInputComponent {
+  enteredPersonName = '';
+
+  onCreatePerson() {
+    console.log(`Created a person ${this.enteredPersonName}`);
+    this.enteredPersonName = '';
+  }
+}
